0ccce386de94a71e4c2bce2a1410857abd98f75d
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-02-19 14:48:57 +0700

    init commit

---
+++ formutama.pas
@@ -0,0 +1,75 @@
+unit FormUtama;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, Buttons,
+  StdCtrls;
+
+type
+
+  { TFormMaster }
+
+  TFormMaster = class(TForm)
+    BtnClose: TButton;
+    BtnStart: TButton;
+    BtnStop: TButton;
+    ImageUtama: TImage;
+    procedure BtnStartClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+    CenterPos: TPoint;
+  public
+    procedure Ellipse(point: TPoint; radius: LongInt; pnColor: TColor);
+    function CartesiusToMonitor(point: TPoint): TPoint;
+  end;
+
+var
+  FormMaster: TFormMaster;
+
+implementation
+
+{$R *.lfm}
+
+{ TFormMaster }
+
+procedure TFormMaster.Ellipse(point: TPoint; radius: LongInt; pnColor: TColor);
+var
+  pointMonitor: TPoint;
+begin
+  ImageUtama.Canvas.Pen.Color:= pnColor;
+  ImageUtama.Canvas.Pen.Width:= 1;
+  pointMonitor:= CartesiusToMonitor(point);
+  ImageUtama.Canvas.Ellipse(pointMonitor.x-radius,pointMonitor.y-radius, pointMonitor.x+radius, pointMonitor.y+radius);
+end;
+
+procedure TFormMaster.FormCreate(Sender: TObject);
+begin
+  CenterPos.x:= ImageUtama.Width div 2;
+  CenterPos.y:= ImageUtama.Height div 2;
+  ImageUtama.Canvas.Brush.Color:= clWhite;
+  ImageUtama.Canvas.Rectangle(0, 0, ImageUtama.Width, ImageUtama.Height);
+end;
+
+procedure TFormMaster.BtnStartClick(Sender: TObject);
+var
+  point: TPoint;
+begin
+  ImageUtama.Canvas.Brush.Color:= clRed;
+  point.SetLocation(0,0);
+  Ellipse(point, 100, clRed);
+end;
+
+function TFormMaster.CartesiusToMonitor(point: TPoint): TPoint;
+var
+  tempPoint: TPoint;
+begin
+  tempPoint.x:= CenterPos.x + point.x;
+  tempPoint.y:= CenterPos.y - point.y;
+  CartesiusToMonitor:= tempPoint;
+end;
+
+end.
+
---
+++ solar_system.lpr
@@ -0,0 +1,22 @@
+program solar_system;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Interfaces, // this includes the LCL widgetset
+  Forms, FormUtama
+  { you can add units after this };
+
+{$R *.res}
+
+begin
+  RequireDerivedFormResource:=True;
+  Application.Scaled:=True;
+  Application.Initialize;
+  Application.CreateForm(TFormMaster, FormMaster);
+  Application.Run;
+end.
+
