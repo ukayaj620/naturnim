d83faf8d13357d2780f8c6704be40b10948acde9
ferdyferdy9 <ferdy.nicolas@gmail.com>
2020-04-08 17:32:20 +0700

    Create New Form

---
+++ formterrain.pas
@@ -0,0 +1,26 @@
+unit FormTerrain;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+  TFormMatrix = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  FormMatrix: TFormMatrix;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
